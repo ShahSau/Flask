8dce0b2b79a1543b0d6a29ac097094a8345ed9e0
ShahSau <shahriarksaurov@gmail.com>
2021-02-08 17:56:26 +0200

    update

--- Codecademy/Travel Sites/travelSites/app.py
+++ Codecademy/Travel Sites/travelSites/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_login import current_user, login_user, logout_user, login_required
 
 
 app = Flask(__name__)
@@ -9,4 +10,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 login = LoginManager(app)
+login.login_view = 'login'
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 import routes, models
