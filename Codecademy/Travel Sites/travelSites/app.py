e74f5c1607e435cda6a8711edad773c597af42e0
ShahSau <shahriarksaurov@gmail.com>
2021-02-08 16:51:27 +0200

    travel

---
+++ Codecademy/Travel Sites/travelSites/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///travel_database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+login = LoginManager(app)
+import routes, models
