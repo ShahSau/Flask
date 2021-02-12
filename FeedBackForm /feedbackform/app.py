746a9a84d6648fb68a777dde106d763e31f04a47
ShahSau <shahriarksaurov@gmail.com>
2021-02-12 14:28:22 +0200

    update

--- FeedBackForm /feedbackform/app.py
+++ FeedBackForm /feedbackform/app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request,url_for
 from flask_fontawesome import FontAwesome
+from flask_sqlalchemy import SQLAlchemy
 
 
-app = Flask(__name__,template_folder='../templates',static_folder='../static')
+app = Flask(__name__)
 fa = FontAwesome(app)
 
 @app.route('/')
@@ -13,9 +14,14 @@ def index():
 def submit():
     if request.method == 'POST':
         customer = request.form['customer']
-        dealer = request.form['dealer']
-        rating = request.form['rating']
-        comments = request.form['comments']
+        email=request.form['email']
+        guest=request.form['guest']
+        room=request.form['room']
+        arrival=request.form['arrival']
+        departure=request.form['departure']
+        comments=request.form['comments']
+        if customer == '' or guest == '' or arrival == '':
+            return render_template('index.html', message= 'Please enter requird fields')
         return render_template('success.html')
 
 if __name__ == '__main__':
