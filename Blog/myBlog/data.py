e77beb828a4b1f60d20e85e7258c49a8df0409bd
ShahSau <shahriarksaurov@gmail.com>
2021-02-20 14:33:11 +0200

    update

---
+++ Blog/myBlog/data.py
@@ -0,0 +1,29 @@
+def Articles():
+    articles= [
+        {
+            'id':1,
+            'title': 'Article 1',
+            'subtitle': 'Small details',
+            'body': 's simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop pu',
+            'author': 'Shahriar',
+            'create_date': '1-12-2020'
+        },
+        {
+            'id':2,
+            'title': 'Article 2',
+            'subtitle': 'Small details2',
+            'body': 's simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop pu',
+            'author': 'Shahriar2',
+            'create_date': '12-12-2020'
+        },
+        {
+            'id':3,
+            'title': 'Article 3',
+            'subtitle': 'Small deta333ils',
+            'body': 's simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop pu',
+            'author': 'Shahriar33',
+            'create_date': '13-12-2020'
+        },
+    ]
+
+    return articles
