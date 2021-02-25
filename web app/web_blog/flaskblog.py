fc9917a90f055d09225c86504f9e74d7cedfe188
ShahSau <shahriarksaurov@gmail.com>
2021-02-25 12:58:23 +0200

    update

--- web app/web_blog/flaskblog.py
+++ web app/web_blog/flaskblog.py
@@ -1,16 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
+posts =[
+    {
+        'author': 'Shah',
+        'title': 'post 1',
+        'content': 'Content fisrt post',
+        'date_posted': 'April 20,2020'
+    }, 
+     {
+        'author': 'Sau',
+        'title': 'post 2',
+        'content': 'Content second post',
+        'date_posted': 'June 20,2020'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Hello world!!</h1>"
+    return render_template("home.html", posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About</h1>"
+    return render_template("about.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
