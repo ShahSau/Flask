7f6526450d59b87386d7023e3540fbcf653f6120
ShahSau <shahriarksaurov@gmail.com>
2021-02-26 23:30:33 +0200

    update

--- web app/web_blog/flaskblog/forms.py
+++ web app/web_blog/flaskblog/forms.py
@@ -1,4 +1,7 @@
 from flask_wtf import FlaskForm
+#for image
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import Form, StringField,TextAreaField, PasswordField,validators, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
@@ -29,3 +32,27 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember me')
 
     submit = SubmitField('Login')
+
+
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',validators=[DataRequired(),Length(min=4, max=25)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile picture', validators=[FileAllowed(['jpg', 'png', 'jpeg'])])
+    
+    submit = SubmitField('Update')
+
+ 
+    ##validation
+    def validate_username(self,username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Username is already taken, please choose another one.')
+
+    def validate_email(self,email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Email is already taken, please choose another one.')
--- web app/web_blog/flaskblog/routes.py
+++ web app/web_blog/flaskblog/routes.py
@@ -1,6 +1,10 @@
+import secrets
+import os
+#for put a limit on pixel in the image
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -67,8 +71,44 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+def save_picture(form_picture):
+    #conver picture name into a 8 bites long name
+    random_hex = secrets.token_hex(8)
+    #to save the pics with the same extention as it was uploaded
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    #where to save picture
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    #reducing the pic pixel
+    output_size= (125,125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
 
-@app.route('/account')
+    i.save(picture_path)
+
+    return picture_fn
+
+
+
+
+
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title="Account")
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated', 'suucess')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+
+    image_file= url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title="Account", image_file=image_file, form=form)
