b8840a3a8bf449a45a5d8b867fcbb5a0a9e50072
ShahSau <shahriarksaurov@gmail.com>
2021-03-01 16:08:22 +0200

    Pagination

--- web app/web_blog/flaskblog/routes.py
+++ web app/web_blog/flaskblog/routes.py
@@ -12,7 +12,9 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route("/")
 @app.route("/home")
 def home():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    #we will use paginate method
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page,per_page=5)
     return render_template("home.html", posts=posts)
 
 @app.route("/about")
@@ -71,8 +73,6 @@ def save_picture(form_picture):
     i.save(picture_path)
 
     return picture_fn
-
-
 
 
 
@@ -145,3 +145,12 @@ def delete_post(post_id):
     db.session.commit()
     flash('Your post has been deleted!', 'danger')
     return redirect(url_for('home'))
+
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    #we will use paginate method
+    posts = Post.query.filter_by(author=user).order_by(Post.date_posted.desc()).paginate(page=page,per_page=5)
+    return render_template("user_posts.html", posts=posts,user=user)
