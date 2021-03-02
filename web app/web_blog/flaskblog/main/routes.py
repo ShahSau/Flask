aef7c026f905e2bb191e285d2521f82d783b1bfd
ShahSau <shahriarksaurov@gmail.com>
2021-03-02 23:30:00 +0200

    update

--- web app/web_blog/flaskblog/main/routes.py
+++ web app/web_blog/flaskblog/main/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, request, Blueprint
 from flaskblog.models import Post
+
 main = Blueprint('main', __name__)
 
 
@@ -7,10 +8,10 @@ main = Blueprint('main', __name__)
 @main.route("/home")
 def home():
     page = request.args.get('page', 1, type=int)
-    #we will use paginate method
-    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page,per_page=5)
-    return render_template("home.html", posts=posts)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template('home.html', posts=posts)
+
 
 @main.route("/about")
 def about():
-    return render_template("about.html", title="about")
+    return render_template('about.html', title='About')
--- web app/web_blog/flaskblog/posts/routes.py
+++ web app/web_blog/flaskblog/posts/routes.py
@@ -8,25 +8,27 @@ from flaskblog.posts.forms import PostForm
 posts = Blueprint('posts', __name__)
 
 
-@posts.route("/post/new",methods=['GET', 'POST'])
+@posts.route("/post/new", methods=['GET', 'POST'])
 @login_required
 def new_post():
     form = PostForm()
     if form.validate_on_submit():
-        post= Post(title=form.title.data,content=form.content.data, author=current_user)
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
         db.session.add(post)
         db.session.commit()
-        flash('Your post have been created!!', 'success')
-        return redirect(url_for('home'))
-    return render_template('create_post.html', title="New Post", form=form,legend='New Post')
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('main.home'))
+    return render_template('create_post.html', title='New Post',
+                           form=form, legend='New Post')
 
 
-@posts.route("/post/<post_id>")
+@posts.route("/post/<int:post_id>")
 def post(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template('post.html', title=post.title, post=post)
 
-@posts.route("/post/<post_id>/update",methods=['GET', 'POST'])
+
+@posts.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
 @login_required
 def update_post(post_id):
     post = Post.query.get_or_404(post_id)
@@ -38,14 +40,15 @@ def update_post(post_id):
         post.content = form.content.data
         db.session.commit()
         flash('Your post has been updated!', 'success')
-        return redirect(url_for('post', post_id=post.id))
+        return redirect(url_for('posts.post', post_id=post.id))
     elif request.method == 'GET':
-
         form.title.data = post.title
         form.content.data = post.content
-    return render_template('create_post.html', title="Updated Post", form=form, legend='Update Post')
+    return render_template('create_post.html', title='Update Post',
+                           form=form, legend='Update Post')
 
-@posts.route("/post/<post_id>/delete",methods=['POST'])
+
+@posts.route("/post/<int:post_id>/delete", methods=['POST'])
 @login_required
 def delete_post(post_id):
     post = Post.query.get_or_404(post_id)
@@ -53,5 +56,5 @@ def delete_post(post_id):
         abort(403)
     db.session.delete(post)
     db.session.commit()
-    flash('Your post has been deleted!', 'danger')
-    return redirect(url_for('home'))
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('main.home'))
