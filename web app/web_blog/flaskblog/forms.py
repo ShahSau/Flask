59a2de53a875d2ba97d522f90f2acfcc12a5424d
ShahSau <shahriarksaurov@gmail.com>
2021-03-02 12:54:51 +0200

    reset email

--- web app/web_blog/flaskblog/forms.py
+++ web app/web_blog/flaskblog/forms.py
@@ -62,3 +62,19 @@ class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
+
+
+ #forms for password reset
+class RequestResetForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password reset')
+
+    def validate_email(self,email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with this email. Please register.')
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset password')
--- web app/web_blog/flaskblog/routes.py
+++ web app/web_blog/flaskblog/routes.py
@@ -3,10 +3,11 @@ import os
 #for put a limit on pixel in the image
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
-from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
+from flaskblog import app, db, bcrypt, mail
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm, RequestResetForm, ResetPasswordForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
+from flask_mail import Message
 
 
 @app.route("/")
@@ -58,7 +59,7 @@ def logout():
     return redirect(url_for('home'))
 
 def save_picture(form_picture):
-    #conver picture name into a 8 bites long name
+    #convert picture name into a 8 bites long name
     random_hex = secrets.token_hex(8)
     #to save the pics with the same extention as it was uploaded
     _, f_ext = os.path.splitext(form_picture.filename)
@@ -154,3 +155,44 @@ def user_posts(username):
     #we will use paginate method
     posts = Post.query.filter_by(author=user).order_by(Post.date_posted.desc()).paginate(page=page,per_page=5)
     return render_template("user_posts.html", posts=posts,user=user)
+
+#where reset password should be send
+def send_reset_email(user):
+    token = user.get_reset_token()
+    msg = Message('Password Reset Request', sender='noreply@demo.com', recipients=[user.email])
+    msg.body = f'''To reset your password visit the link:
+{url_for('reset_token', token=token, _external=True)}
+If you didnot make this resquest then discard this email.
+    '''
+    mail.send(msg)
+
+@app.route("/reset_password",methods=['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        send_reset_email(user)
+        flash('An email has been sent with instruction to reset your password','info')
+        return redirect(url_for('login'))
+    return render_template('reset_request.html', title= 'Reset Password', form=form)
+
+
+
+@app.route("/reset_password/<token>",methods=['GET', 'POST'])
+def reset_token(token):
+     if current_user.is_authenticated:
+        return redirect(url_for('home'))
+     user = User.verify_reset_token(token)
+     if user is None:
+         flash('That is an invalid or expired token.', 'warning')
+         return redirect(url_for('reset_request'))
+     form = ResetPasswordForm()
+     if form.validate_on_submit():
+         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+         user.password = hashed_password
+         db.session.commit()
+         flash('Your password has been updated. you can now login.','success')
+         return redirect(url_for('login'))
+     return render_template('reset_token.html', title= 'Reset Password', form=form)
