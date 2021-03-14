cb9ad20b70a6543d74a0cbf8943cd39c5b46736b
Shahriar Karim Saurov <shahriarkarimsaurov@kamk.fi>
2021-03-14 22:58:59 +0200

    Docker

--- Weather app/weatherApp/app.py
+++ Weather app/weatherApp/app.py
@@ -106,3 +106,6 @@ def delete_city(name):
 
     flash(f'Successfully deleted { city.name }', 'success')
     return redirect(url_for('index_get'))
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
